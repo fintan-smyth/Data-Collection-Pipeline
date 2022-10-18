a5a01763e318fbaa98da6566243c17705a993789
fintan-smyth <fintan.smyth@ymail.com>
2022-10-18 13:18:14 +0000

    Data storage added

--- data_collection/scraper.py
+++ data_collection/scraper.py
@@ -1,12 +1,18 @@
+from datetime import datetime
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
+import boto3
 import json
 import os
 import requests
+import shutil
 import time
 import uuid
 
@@ -34,6 +40,7 @@ class scraper:
         See help(scraper) for accurate signature.
         '''
         self.driver = webdriver.Firefox()
+        self.film_data_dic_list = []
         self.start_page = start_page
         self.start_url = f"https://letterboxd.com/films/popular/page/{self.start_page}"
         self.driver.get(self.start_url)
@@ -75,6 +82,17 @@ class scraper:
         film_data_dic[element] = scraped_text
         print(f'{element}: {scraped_text}')
 
+    def __scrape_film_stat_element(self, film_data_dic: dict, element: str, xpath: str):
+
+        delay = 10
+        driver = self.driver
+
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        stat_container = driver.find_element(by=By.XPATH, value=xpath)
+        stat = stat_container.get_attribute('data-original-title').split()[2]
+        film_data_dic[f'{element}'] = stat
+        print(f'{element}: {stat}')
+
     def __scrape_text_data(self, film_data_dic: dict):
 
         delay = 10
@@ -84,9 +102,16 @@ class scraper:
         self.__scrape_text_element(film_data_dic, 'Year', '//a[starts-with(@href,"/films/year/")]')
         self.__scrape_text_element(film_data_dic, 'Runtime', '//p[@class="text-link text-footer"]')
         self.__scrape_text_element(film_data_dic, 'Rating', '//a[starts-with(@class,"tooltip display-rating")]')
-        self.__scrape_text_element(film_data_dic, 'Watches', '//a[@class="has-icon icon-watched icon-16 tooltip"]')
-        self.__scrape_text_element(film_data_dic, 'Lists', '//a[@class="has-icon icon-list icon-16 tooltip"]')
-        self.__scrape_text_element(film_data_dic, 'Likes', '//a[@class="has-icon icon-like icon-liked icon-16 tooltip"]')
+
+        self.__scrape_film_stat_element(film_data_dic, 'Watches', '//a[@class="has-icon icon-watched icon-16 tooltip"]')
+        self.__scrape_film_stat_element(film_data_dic, 'Lists', '//a[@class="has-icon icon-list icon-16 tooltip"]')
+        self.__scrape_film_stat_element(film_data_dic, 'Likes', '//a[@class="has-icon icon-like icon-liked icon-16 tooltip"]')
+
+        # WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//a[@class="has-icon icon-watched icon-16 tooltip"]')))
+        # watches_container = driver.find_element(by=By.XPATH, value='//a[@class="has-icon icon-watched icon-16 tooltip"]')
+        # watches = watches_container.get_attribute('data-original-title').split()[2]
+        # film_data_dic['Watches'] = watches
+        # print(f'Watches: {watches}')
         
         WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//a[starts-with(@href,"/director/")]')))
         director = driver.find_element(by=By.XPATH, value='//a[starts-with(@href,"/director/")]').text
@@ -104,27 +129,116 @@ class scraper:
             film_data_dic['Top 250 Position'] = top_250_pos
             print(f'Top 250 position: {top_250_pos}')
         except:
-            film_data_dic['Top 250 Position'] = 'N/A'
-
+            film_data_dic['Top 250 Position'] = np.nan
         WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//div[@class="review body-text -prose -hero prettify"]')))
         try:
             more_button = driver.find_element(by=By.XPATH, value='//span[@class="condense_control condense_control_more"]')
             more_button.click()
             description = driver.find_element(by=By.XPATH, value='//div[@class="truncate condenseable"]').text.split('×')[0]   
         except:
-            description = driver.find_element(by = By.XPATH, value='//div[@class="review body-text -prose -hero prettify"]//p').text
+            description = driver.find_element(by=By.XPATH, value='//div[@class="review body-text -prose -hero prettify"]//p').text
         film_data_dic['Description'] = description
         print(f'Description: {description}')
     
+    def __clean_scraped_data(self, film_data_dic: dict) -> dict:
+        film_data_dic['Year'] = int(film_data_dic['Year'])
+        film_data_dic['Runtime'] = int(film_data_dic['Runtime'].split()[0])
+        film_data_dic['Rating'] = float(film_data_dic['Rating'])
+        watches = film_data_dic['Watches'].replace(',', '')
+        film_data_dic['Watches'] = int(watches)
+        lists = film_data_dic['Lists'].replace(',', '')
+        film_data_dic['Lists'] = int(lists)
+        likes = film_data_dic['Likes'].replace(',', '')
+        film_data_dic['Likes'] = int(likes)
+        director = film_data_dic['Director']
+        if len(director) == 2:
+            film_data_dic['Director'] = f'{director[0]}, {director[1]}'
+        elif len(director) == 3:
+            film_data_dic['Director'] = f'{director[0]}, {director[1]}, {director[2]}'
+        else:
+            pass
+        try:
+            film_data_dic['Top 250 Position'] = int(film_data_dic['Top 250 Position'])
+        except:
+            pass
+        return film_data_dic
+
+    def __store_raw_data_local(self, film_data_dic: dict):
+        '''
+        Stores scraped data for single film locally.
+
+        Parameters
+        ----------
+        film_data_dic: dict
+            A dictionary containing all scraped data for a single film.
+        '''
+        friendly_id = film_data_dic['friendly_id']
+        film_data_dic['data_obtained_time'] = str(film_data_dic['data_obtained_time'])
+        try:
+            os.mkdir('raw_data')
+        except:
+            pass
+        try:
+            os.mkdir(f'raw_data/{friendly_id}')
+        except:
+            pass      
+        with open(f'raw_data/{friendly_id}/data.json', 'w') as film_data_dic_file:
+            json.dump(film_data_dic, film_data_dic_file)      
+        image_request = requests.get(film_data_dic['Poster'])
+        try:
+            os.mkdir(f'raw_data/{friendly_id}/images')
+        except:
+            pass
+        with open(f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg', 'wb') as image_file:
+            image_file.write(image_request.content)
+
+    def __store_raw_data_s3(self, film_data_dic: dict):
+        '''
+        Stores scraped data for single film in an s3 bucket.
+
+        Parameters
+        ----------
+        film_data_dic: dict
+            A dictionary containing all scraped data for a single film.
+        '''
+        friendly_id = film_data_dic['friendly_id']
+
+        s3_client = boto3.client('s3')
+        s3_client.upload_file(f'raw_data/{friendly_id}/data.json', 'letterboxd-data-bucket', f'raw_data/{friendly_id}/data.json')
+        s3_client.upload_file(f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg', 'letterboxd-data-bucket', f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg')
+
+    def __store_tabular_data_rds(self):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = 'letterboxd-db.c4dnzzretdoh.eu-west-2.rds.amazonaws.com'
+        USER = 'postgres'
+        PASSWORD = 'password'
+        PORT = 5432
+        DATABASE = 'postgres'
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+
+        film_data_df = pd.DataFrame.from_dict(self.film_data_dic_list).set_index('friendly_id')
+        film_data_df['Top 250 Position'] = film_data_df['Top 250 Position'].astype('Int64')
+        film_data_df.to_sql('film_data', engine, if_exists='append')
+        # print(film_data_df.info(verbose=True))
+        # df = pd.read_sql_table('film_data', engine)
+        # print(df)
+
+    def __save_tabular_data_csv(self):
+        film_data_df = pd.DataFrame.from_dict(self.film_data_dic_list).set_index('friendly_id')
+        film_data_df['Top 250 Position'] = film_data_df['Top 250 Position'].astype('Int64')
+        output_path='film_data.csv'
+        film_data_df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+
     def accept_cookies(self):
         '''
         Closes the 'accept cookies' pop-up.
         '''
         delay = 10
         try:
-            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="banner_consent--2qj6F"][2]')))
+            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="fc-button fc-cta-consent fc-primary-button"]')))
             print('Accept cookies button ready...')
-            accept_cookies_button = self.driver.find_element(by=By.XPATH, value='//*[@class="banner_consent--2qj6F"][2]')
+            accept_cookies_button = self.driver.find_element(by=By.XPATH, value='//*[@class="fc-button fc-cta-consent fc-primary-button"]')
             accept_cookies_button.click()
             print('Cookies accepted.\n')
             return True
@@ -175,68 +289,104 @@ class scraper:
         film_data_dic: dict
             A dictionary containing all scraped data for a single film.
         '''
-        film_data_dic = {}
-        driver = self.driver
-        driver.get(link)
+        while True:
+            film_data_dic = {}
+            driver = self.driver
+            driver.get(link)
 
-        friendly_id = link.split('/')[4]
-        print(f'friendly_id: {friendly_id}')
-        film_data_dic['friendly_id'] = friendly_id
+            friendly_id = link.split('/')[4]
+            print(f'friendly_id: {friendly_id}')
+            film_data_dic['friendly_id'] = friendly_id
 
-        film_uuid = uuid.uuid4()
-        print(f'UUID: {film_uuid}')
-        film_data_dic['UUID'] = str(film_uuid)
+            film_uuid = uuid.uuid4()
+            print(f'UUID: {film_uuid}')
+            film_data_dic['UUID'] = str(film_uuid)
 
-        self.__scrape_text_data(film_data_dic)
+            self.__scrape_text_data(film_data_dic)
+            if film_data_dic['Description'] != '':
+                break
+            print("\nFailed to scrape 'Description'. Reloading link...\n")
         self.__scrape_image_data(film_data_dic)
+
+        timestamp = datetime.now()
+        print(f'data_obtained_time: {timestamp}')
+        film_data_dic['data_obtained_time'] = timestamp
+
+
+        film_data_dic = self.__clean_scraped_data(film_data_dic)
+        self.film_data_dic_list.append(film_data_dic)
         print('\n')
         time.sleep(1)
 
         return film_data_dic
 
-    def store_scraped_data(self, film_data_dic: dict):
+    def store_raw_scraped_data(self, film_data_dic: dict):
         '''
-        Stores scraped data for single film locally.
+        Saves the scraped raw data for a single film locally and uploads it to an s3 bucket.
 
         Parameters
         ----------
         film_data_dic: dict
             A dictionary containing all scraped data for a single film.
         '''
-        friendly_id = film_data_dic['friendly_id']
-        try:
-            os.mkdir('raw_data')
-        except:
-            pass
-        try:
-            os.mkdir(f'raw_data/{friendly_id}')
-        except:
-            pass      
-        with open(f'raw_data/{friendly_id}/data.json', 'w') as film_data_dic_file:
-            json.dump(film_data_dic, film_data_dic_file)      
-        image_request = requests.get(film_data_dic['Poster'])
-        try:
-            os.mkdir(f'raw_data/{friendly_id}/images')
-        except:
-            pass
-        with open(f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg', 'wb') as image_file:
-            image_file.write(image_request.content)
+        self.__store_raw_data_local(film_data_dic)
+        self.__store_raw_data_s3(film_data_dic)
 
+    def data_storage_options_prompt(self):
+        '''
+        Prompts the user for how they would like to store their data.
+        '''
+        while True:
+            rds_reply = input('\nUpload tabular data to AWS RDS? (Y/N)').lower().strip()
+            if rds_reply[0] == 'y':
+                self.__store_tabular_data_rds()
+                print('\nTabular data uploaded to RDS')
+                break
+            elif rds_reply[0] == 'n':
+                break
+            else:
+                print('\nPlease choose yes or no.')
+        while True:
+            csv_reply = input('\nSave tabular data locally as .csv? (Y/N)').lower().strip()
+            if csv_reply[0] == 'y':
+                self.__save_tabular_data_csv()
+                print('\nTabular data saved to film_data.csv')
+                break
+            elif csv_reply[0] == 'n':
+                break
+            else:
+                print('\nPlease choose yes or no.')
+        while True:
+            raw_data_reply = input('\nKeep local copy of raw data? (Y/N)').lower().strip()
+            if raw_data_reply[0] == 'y':
+                print("\nRaw data saved in 'raw_data' folder")
+                break
+            elif raw_data_reply[0] == 'n':
+                shutil.rmtree('raw_data', ignore_errors=True)
+                print('\nLocal copy of raw data removed')
+                break
+            else:
+                print('\nPlease choose yes or no.')
 
+        
 if __name__ == "__main__":
-    lbox_scrape = scraper(start_page = 1)
-    lbox_scrape.accept_cookies()
-    # link_list = lbox_scrape.get_film_links(pages = 1)
-    # # link_list = ['https://letterboxd.com/film/spider-man-into-the-spider-verse/', 'https://letterboxd.com/film/ratatouille/', 'https://letterboxd.com/film/lady-bird/', 'https://letterboxd.com/film/dune-2021/', 'https://letterboxd.com/film/the-grand-budapest-hotel/', 'https://letterboxd.com/film/once-upon-a-time-in-hollywood/', 'https://letterboxd.com/film/la-la-land/', 'https://letterboxd.com/film/whiplash-2014/', 'https://letterboxd.com/film/avengers-infinity-war/', 'https://letterboxd.com/film/the-wolf-of-wall-street/', 'https://letterboxd.com/film/everything-everywhere-all-at-once/', 'https://letterboxd.com/film/the-shining/']
-    # for links in link_list:
-    #     try:
-    #         film_data_dic = lbox_scrape.scrape_data_from_film_entry(links)
-    #         # lbox_scrape.store_scraped_data(film_data_dic) 
-    #     except:
-    #         print('\nAborted\n')
-    #         break
-    lbox_scrape.driver.quit()
-    # help(scraper)
+    lbox_scraper = scraper(start_page = 1)
+    lbox_scraper.accept_cookies()
+    # link_list = lbox_scraper.get_film_links(pages = 1)
+    # link_list = ['https://letterboxd.com/film/spider-man-into-the-spider-verse/', 'https://letterboxd.com/film/ratatouille/', 'https://letterboxd.com/film/lady-bird/', 'https://letterboxd.com/film/dune-2021/', 'https://letterboxd.com/film/the-grand-budapest-hotel/', 'https://letterboxd.com/film/once-upon-a-time-in-hollywood/', 'https://letterboxd.com/film/la-la-land/', 'https://letterboxd.com/film/whiplash-2014/', 'https://letterboxd.com/film/avengers-infinity-war/', 'https://letterboxd.com/film/the-wolf-of-wall-street/', 'https://letterboxd.com/film/everything-everywhere-all-at-once/', 'https://letterboxd.com/film/the-shining/']
+    link_list = ['https://letterboxd.com/film/ratatouille/', 'https://letterboxd.com/film/avengers-infinity-war/']
+    for link in link_list:
+        try:
+            film_data_dic = lbox_scraper.scrape_data_from_film_entry(link)
+            lbox_scraper.store_raw_scraped_data(film_data_dic)
+        except:
+            print('\nAborted\n')
+            break
+    
+    lbox_scraper.driver.quit()
+    lbox_scraper.data_storage_options_prompt()
+
+
 
 
 
--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,4 +1,7 @@
 from data_collection import scraper
+from sqlalchemy import create_engine, delete
+import pandas as pd
+import boto3
 import os
 import random
 import shutil
@@ -8,12 +11,12 @@ class scraperTestCase(unittest.TestCase):
     def setUp(self):
         self.scrapetest = scraper.scraper()
 
-    # @unittest.skip
+    @unittest.skip
     def test_accept_cookies(self):
         accepted = self.scrapetest.accept_cookies()
         self.assertTrue(accepted)
 
-    # @unittest.skip
+    @unittest.skip
     def test_get_film_links(self):
         pages = 3
         self.scrapetest.accept_cookies()
@@ -21,7 +24,7 @@ class scraperTestCase(unittest.TestCase):
         n_links = len(link_list)
         self.assertEqual(n_links, pages*72)
     
-    # @unittest.skip
+    @unittest.skip
     def test_scrape_data_from_film_entry(self):
         self.scrapetest.accept_cookies()
         link_list = ['https://letterboxd.com/film/spider-man-into-the-spider-verse/', 
@@ -36,14 +39,53 @@ class scraperTestCase(unittest.TestCase):
                      'https://letterboxd.com/film/the-wolf-of-wall-street/', 
                      'https://letterboxd.com/film/everything-everywhere-all-at-once/', 
                      'https://letterboxd.com/film/the-shining/']
-        film_link = random.choice(link_list)
+        # film_link = random.choice(link_list)
+        film_link = 'https://letterboxd.com/film/avengers-infinity-war/'
         film_data = self.scrapetest.scrape_data_from_film_entry(film_link)
-        self.assertTrue(len(film_data) == 13)
+        self.assertTrue(len(film_data) == 14)
         for key in film_data:
             self.assertTrue(len(film_data[key]) > 0)
     
+    @unittest.skip
+    def test_store_raw_scraped_data(self):
+        self.scrapetest.accept_cookies()
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket('letterboxd-data-bucket')
+        film_data_dic = {"friendly_id": "testfilm",
+                         "UUID": "1234-5678-9012-3456", 
+                         "Title": "Test Film", 
+                         "Year": "1998", 
+                         "Runtime": "420 mins", 
+                         "Rating": "5.0", 
+                         "Watches": "999K", 
+                         "Lists": "100K", 
+                         "Likes": "10M", 
+                         "Director": "Fintan Smyth", 
+                         "Top 250 Position": "1", 
+                         "Description": "Test description",
+                         "Poster": "https://a.ltrbxd.com/resized/sm/upload/dp/sq/oj/cg/hNjxJbejPHSmKVidHQ9ZHaC0Z7r-0-230-0-345-crop.jpg?v=f0ff67d1b9",
+                         "data_obtained_time": "2020-01-01 16:20:00"}
+        self.scrapetest.store_raw_scraped_data(film_data_dic)
+        self.assertTrue(os.access('raw_data/testfilm/data.json', os.F_OK))
+        self.assertTrue(os.access('raw_data/testfilm/images/testfilm_poster.jpg', os.F_OK))
+        objects = []
+        for file in bucket.objects.filter(Prefix='raw_data/testfilm/'):
+            objects.append(file.key)
+        self.assertTrue(len(objects) == 2)
+        bucket.objects.filter(Prefix='raw_data/testfilm/').delete()
+        shutil.rmtree('raw_data/testfilm')
+
     # @unittest.skip
-    def test_store_scraped_data(self):
+    def test_data_storage_options_prompt(self):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = 'letterboxd-db.c4dnzzretdoh.eu-west-2.rds.amazonaws.com'
+        USER = 'postgres'
+        PASSWORD = 'password'
+        PORT = 5432
+        DATABASE = 'postgres'
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+
         self.scrapetest.accept_cookies()
         film_data_dic = {"friendly_id": "testfilm",
                          "UUID": "1234-5678-9012-3456", 
@@ -57,13 +99,23 @@ class scraperTestCase(unittest.TestCase):
                          "Director": "Fintan Smyth", 
                          "Top 250 Position": "1", 
                          "Description": "Test description",
-                         "Poster": "https://a.ltrbxd.com/resized/sm/upload/dp/sq/oj/cg/hNjxJbejPHSmKVidHQ9ZHaC0Z7r-0-230-0-345-crop.jpg?v=f0ff67d1b9"}
-        self.scrapetest.store_scraped_data(film_data_dic)
-        self.assertTrue(os.access('raw_data/testfilm/data.json', os.F_OK))
-        self.assertTrue(os.access('raw_data/testfilm/images/testfilm_poster.jpg', os.F_OK))
-        shutil.rmtree('raw_data/testfilm')
+                         "Poster": "https://a.ltrbxd.com/resized/sm/upload/dp/sq/oj/cg/hNjxJbejPHSmKVidHQ9ZHaC0Z7r-0-230-0-345-crop.jpg?v=f0ff67d1b9",
+                         "data_obtained_time": "2020-01-01 16:20:00"}
+        self.scrapetest.film_data_dic_list.append(film_data_dic)
+        self.scrapetest.data_storage_options_prompt()
+        self.assertTrue(os.access('film_data.csv', os.F_OK))
+        csv_df = pd.read_csv('film_data.csv', index_col=0)
+        csv_df = csv_df.drop(index='testfilm')
+        csv_df.to_csv('film_data.csv')
+        rds_df = pd.read_sql_table('film_data', engine)
+        print(rds_df.loc[rds_df['friendly_id'] == 'testfilm'])
+        # finish after next prereq content
+
+
+
+
 
     def tearDown(self):
         self.scrapetest.driver.quit()
 
-unittest.main(argv=[''], verbosity=2, exit=False)
+unittest.main(argv=[''], verbosity=3, exit=False)
