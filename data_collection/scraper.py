89541bef4dc47208b1c6cc3c03c6489adaff3d48
fintan-smyth <fintan.smyth@ymail.com>
2022-10-26 17:13:43 +0000

    ACTUALLY fixed merging mistakes

--- data_collection/scraper.py
+++ data_collection/scraper.py
@@ -19,12 +19,7 @@ import uuid
 
 class scraper:
     '''
-    A webscraper used to retrieve film data from the 'Popular' section of letterboxd.com.
-
-    Parameters
-    ----------
-    start_page: int
-        The number of the first page in the 'popular' section that will be scraped for links to film entries.
+    A webscraper used to retrieve film data from the 'Popular' section of letterboxd.com
     
     Attributes
     ----------
@@ -36,10 +31,20 @@ class scraper:
         An empty list that will be populated with dictionaries containing film data.
     start_page: int
         The number of the first page in the 'popular' section that the scraper will be scraped for links to film entries (equal to start_page parameter).
+    pages: int
+        The number of pages in the 'popular' section that the scraper will obtain links to film entries from.
+    s3_storage_bool: bool
+        Boolean corresponding to whether data will be stored in an s3 bucket.
+    keep_raw_data_bool: bool
+        Boolean corresponding to whether a local copy of raw data will be kept.
+    rds_bool: bool
+        Boolean corresponding to whether tabular data will be stored in an RDS database.
+    csv_bool: bool
+        Boolean corresponding to whether tabular data will be saved locally as a .csv.
     start_url: str
         The URL of the page the driver will navigate to upon initialisation.
     '''
-    def __init__(self, start_page: int = 1):
+    def __init__(self):
         '''
         See help(scraper) for accurate signature.
         '''
@@ -56,26 +61,28 @@ class scraper:
         PORT = 5432
         DATABASE = 'postgres'
         self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-        self.film_data_dic_list = []
-        self.start_page = start_page
+        
+        while True:
+            start_page_prompt = int(input('Please choose a starting page: '))
+            if start_page_prompt > 0:
+                self.start_page = start_page_prompt
+                break
+            else:
+                print('Please choose a positive integer...')
+        
+        while True:
+            pages_prompt = int(input('Please choose a number of pages to scrape: '))
+            if pages_prompt > 0:
+                self.pages = pages_prompt
+                break
+            else:
+                print('Please choose a positive integer...')
+        self.s3_storage_bool = True
+        self.keep_raw_data_bool = True
+        self.rds_bool = True
+        self.csv_bool = False
         self.start_url = f"https://letterboxd.com/films/popular/page/{self.start_page}"
         self.driver.get(self.start_url)
-
-    def __get_film_links_from_single_page(self) -> list:
-        delay = 10
-        WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="poster-list -p70 -grid"]/li')))
-        print('Poster list ready...')
-        film_container = self.driver.find_element(by=By.XPATH, value='//*[@class="poster-list -p70 -grid"]')
-        film_list = film_container.find_elements(by=By.XPATH, value='./li')
-        link_list = []
-
-        for film in film_list:
-            WebDriverWait(film, delay).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
-            a_tag = film.find_element(By.TAG_NAME, 'a')
-            link = a_tag.get_attribute('href')
-            link_list.append(link)
-        print('Links scraped.\n')
-        return link_list
 
     def __scrape_image_data(self, film_data_dic: dict):
 
@@ -109,7 +116,7 @@ class scraper:
         film_data_dic[f'{element}'] = stat
         # print(f'{element}: {stat}')
 
-    def __scrape_text_data(self, film_data_dic: dict):
+    def __scrape_all_text_data(self, film_data_dic: dict):
 
         delay = 10
         driver = self.driver
@@ -153,7 +160,7 @@ class scraper:
     
     def __clean_scraped_data(self, film_data_dic: dict) -> dict:
         film_data_dic['year'] = int(film_data_dic['year'])
-        film_data_dic['runtime'] = int(film_data_dic['runtime'].split()[0])
+        film_data_dic['runtime'] = int(film_data_dic['runtime'].split()[0].replace(',', ''))
         film_data_dic['rating'] = float(film_data_dic['rating'])
         watches = film_data_dic['watches'].replace(',', '')
         film_data_dic['watches'] = int(watches)
@@ -177,7 +184,6 @@ class scraper:
     def __store_raw_data_local(self, film_data_dic: dict):
         '''
         Stores scraped data for single film locally.
-
         Parameters
         ----------
         film_data_dic: dict
@@ -206,7 +212,6 @@ class scraper:
     def __store_raw_data_s3(self, film_data_dic: dict):
         '''
         Stores scraped data for single film in an s3 bucket.
-
         Parameters
         ----------
         film_data_dic: dict
@@ -214,29 +219,26 @@ class scraper:
         '''
         friendly_id = film_data_dic['friendly_id']
 
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client('s3', 
+                                aws_access_key_id='AKIAWQVNI7VAFIWE6GHP',
+                                aws_secret_access_key= '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')
         s3_client.upload_file(f'raw_data/{friendly_id}/data.json', 'letterboxd-data-bucket', f'raw_data/{friendly_id}/data.json')
         s3_client.upload_file(f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg', 'letterboxd-data-bucket', f'raw_data/{friendly_id}/images/{friendly_id}_poster.jpg')
 
     def __store_tabular_data_rds(self, film_data_dic: dict):
-        if len(film_data_dic) > 0:
-            film_data_df = pd.DataFrame.from_dict(film_data_dic).set_index('friendly_id')
-            film_data_df['top_250_position'] = film_data_df['top_250_position'].astype('Int64')
-            film_data_df.to_sql('film_data', self.engine, if_exists='append')
-            print('\nTabular data uploaded to RDS')
-        else:
-            print('\nNo tabular data to upload')
+        film_data_df = pd.DataFrame([film_data_dic]).set_index('friendly_id')
+        film_data_df['top_250_position'] = film_data_df['top_250_position'].astype('Int64')
+        film_data_df.to_sql('film_data', self.engine, if_exists='append')
+
+    def __remove_local_raw_data(self):
+        shutil.rmtree('raw_data', ignore_errors=True)
         
     def __save_tabular_data_csv(self, film_data_dic: dict):
-        if len(film_data_dic) > 0:
-            film_data_df = pd.DataFrame.from_dict(film_data_dic).set_index('friendly_id')
-            film_data_df['top_250_position'] = film_data_df['top_250_position'].astype('Int64')
-            output_path='film_data.csv'
-            film_data_df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
-            print('\nTabular data saved to to film_data.csv')
-
-        else:
-            print('\nNo tabular data to save locally')
+        film_data_df = pd.DataFrame([film_data_dic]).set_index('friendly_id')
+        film_data_df['top_250_position'] = film_data_df['top_250_position'].astype('Int64')
+        output_path='film_data.csv'
+        film_data_df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+        print('\nTabular data saved to to film_data.csv')
 
     def accept_cookies(self):
         '''
@@ -255,39 +257,26 @@ class scraper:
             return False
         time.sleep(1)
     
-    def get_film_links(self, pages: int) -> list:
-        '''
-        Scrapes the links to film entries from a set number of pages, starting from the 'start_page'.
-
-        Parameters
-        ----------
-        pages: int
-            The number of pages from which to scrape links to film entries.
-        
-        Returns
-        -------
-        link_list: list
-            A list of links to film entries in the letterboxd.com system.
-        '''
+    def get_film_links_from_single_page(self) -> list:
+        delay = 10
+        WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="poster-list -p70 -grid"]/li')))
+        print('Poster list ready...')
+        film_container = self.driver.find_element(by=By.XPATH, value='//*[@class="poster-list -p70 -grid"]')
+        film_list = film_container.find_elements(by=By.XPATH, value='./li')
         link_list = []
-        link_list.extend(self.__get_film_links_from_single_page())
-        
-        next_page = self.start_page + 1
-        for i in range(pages-1):
-            time.sleep(1)
-            next_page_url = f'https://letterboxd.com/films/popular/size/small/page/{next_page}/'
-            self.driver.get(next_page_url)
-            print(f'Page {next_page} loaded.')
-            next_page += 1
-            link_list.extend(self.__get_film_links_from_single_page())
-        
+
+        for film in film_list:
+            WebDriverWait(film, delay).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
+            a_tag = film.find_element(By.TAG_NAME, 'a')
+            link = a_tag.get_attribute('href')
+            link_list.append(link)
+        print('Links scraped.\n')
         return link_list
 
     def check_if_link_already_scraped(self, link: str) -> bool:
         '''
         Checks if there is already data from this link in the RDS database and returns a corresponding boolean.
         Returns True if there is already an entry with the same 'friendly_id' in the RDS database. Returns False if there is not.
-
         Parameters
         ----------
         link: str
@@ -308,7 +297,6 @@ class scraper:
     def scrape_data_from_film_entry(self, link: str) -> dict:
         '''
         Scrapes all chosen data from the page of a film entry and stores it in a dictionary.
-
         Parameters
         ----------
         link: str
@@ -332,7 +320,7 @@ class scraper:
             # print(f'UUID: {film_uuid}')
             film_data_dic['uuid'] = str(film_uuid)
 
-            self.__scrape_text_data(film_data_dic)
+            self.__scrape_all_text_data(film_data_dic)
             if film_data_dic['description'] != '':
                 break
             print("Failed to scrape 'description'. Reloading link...")
@@ -344,78 +332,102 @@ class scraper:
 
 
         film_data_dic = self.__clean_scraped_data(film_data_dic)
-        self.film_data_dic_list.append(film_data_dic)
+        # self.film_data_dic_list.append(film_data_dic)
         # print('\n')
         time.sleep(1)
 
         return film_data_dic
-
-    def store_raw_scraped_data(self, film_data_dic: dict):
-        '''
-        Saves the scraped raw data for a single film locally and uploads it to an s3 bucket.
-
-        Parameters
-        ----------
-        film_data_dic: dict
-            A dictionary containing all scraped data for a single film.
-        '''
-        self.__store_raw_data_local(film_data_dic)
-        self.__store_raw_data_s3(film_data_dic)
 
     def data_storage_options_prompt(self):
         '''
         Prompts the user for how they would like to store their data.
         '''
         while True:
-            rds_reply = input('\nUpload tabular data to AWS RDS? (Y/N)').lower().strip()
-            if rds_reply[0] == 'y':
-                self.__store_tabular_data_rds(self.film_data_dic_list)
+            s3_prompt = input('Store scraped raw data in s3 bucket? (Y/N)').lower().strip()
+            if s3_prompt[0] == 'n':
+                self.s3_storage_bool = False
                 break
-            elif rds_reply[0] == 'n':
-                break
-            else:
-                print('\nPlease choose yes or no.')
-        while True:
-            csv_reply = input('\nSave tabular data locally as .csv? (Y/N)').lower().strip()
-            if csv_reply[0] == 'y':
-                self.__save_tabular_data_csv(self.film_data_dic_list)
-                break
-            elif csv_reply[0] == 'n':
+            elif s3_prompt[0] == 'y':
+                self.s3_storage_bool = True
                 break
             else:
-                print('\nPlease choose yes or no.')
-        while True:
-            raw_data_reply = input('\nKeep local copy of raw data? (Y/N)').lower().strip()
-            if raw_data_reply[0] == 'y':
-                print("\nRaw data saved in 'raw_data' folder")
-                break
-            elif raw_data_reply[0] == 'n':
-                shutil.rmtree('raw_data', ignore_errors=True)
-                print('\nLocal copy of raw data removed')
-                break
-            else:
-                print('\nPlease choose yes or no.')
+                print('Please choose yes or no...')
 
+        while True:
+            keep_raw_prompt = input('Keep local copy of raw data? (Y/N)').lower().strip()
+            if keep_raw_prompt[0] == 'n':
+                self.keep_raw_data_bool = False
+                break
+            elif keep_raw_prompt[0] == 'y':
+                self.keep_raw_data_bool = True
+                break
+            else:
+                print('Please choose yes or no...')
+
+        while True:
+            rds_prompt = input('Store tabular data in RDS database? (Y/N)').lower().strip()
+            if rds_prompt[0] == 'n':
+                self.rds_bool = False
+                break
+            elif rds_prompt[0] == 'y':
+                self.rds_bool = True
+                break
+            else:
+                print('Please choose yes or no...')
+
+        while True:
+            csv_prompt = input('Save tabular data as .csv? (Y/N)').lower().strip()
+            if csv_prompt[0] == 'n':
+                self.csv_bool = False
+                break
+            elif csv_prompt[0] == 'y':
+                self.csv_bool = True
+                break
+            else:
+                print('Please choose yes or no...')
+
+    def implement_data_storage_options(self, film_data_dic: dict):
+        '''
+        Implements the data storage options chosen in the prompt.
+        Parameters
+        ----------
+        film_data_dic: dict
+            A dictionary containing all scraped data for a single film.
+        '''
+        self.__store_raw_data_local(film_data_dic)
+        if self.s3_storage_bool == True:
+            self.__store_raw_data_s3(film_data_dic)
+        if self.keep_raw_data_bool == False:
+            self.__remove_local_raw_data()
+        if self.rds_bool == True:
+            self.__store_tabular_data_rds(film_data_dic)
+        if self.csv_bool == True:
+            self.__save_tabular_data_csv(film_data_dic)
+    
         
 if __name__ == "__main__":
-    lbox_scraper = scraper(start_page = 1)
-    lbox_scraper.accept_cookies()
-    link_list = lbox_scraper.get_film_links(pages = 10)
-    # link_list = ['https://letterboxd.com/film/spider-man-into-the-spider-verse/', 'https://letterboxd.com/film/ratatouille/', 'https://letterboxd.com/film/lady-bird/', 'https://letterboxd.com/film/dune-2021/', 'https://letterboxd.com/film/the-grand-budapest-hotel/', 'https://letterboxd.com/film/once-upon-a-time-in-hollywood/', 'https://letterboxd.com/film/la-la-land/', 'https://letterboxd.com/film/whiplash-2014/', 'https://letterboxd.com/film/avengers-infinity-war/', 'https://letterboxd.com/film/the-wolf-of-wall-street/', 'https://letterboxd.com/film/everything-everywhere-all-at-once/', 'https://letterboxd.com/film/the-shining/']
-    # link_list = ['https://letterboxd.com/film/ratatouille/', 'https://letterboxd.com/film/avengers-infinity-war/']
-    for link in link_list:
-        if lbox_scraper.check_if_link_already_scraped(link) == True:
-            link_id = link.split('/')[4]
-            print(f'Data for {link_id} already exists. Skipping to next link...')
-            continue
-        try:
-            film_data_dic = lbox_scraper.scrape_data_from_film_entry(link)
-            lbox_scraper.store_raw_scraped_data(film_data_dic)
-        except:
-            print('\nAborted')
-            break
-    lbox_scraper.driver.quit()
+    lbox_scraper = scraper()
     lbox_scraper.data_storage_options_prompt()
+    lbox_scraper.accept_cookies()
+    next_page = lbox_scraper.start_page + 1
+    pages = lbox_scraper.pages
+    for i in range(pages):
+        link_list = lbox_scraper.get_film_links_from_single_page()
+        for link in link_list:
+            if lbox_scraper.check_if_link_already_scraped(link) == True:
+                link_id = link.split('/')[4]
+                print(f'Data for {link_id} already exists. Skipping to next link...')
+                continue
+            film_data_dic = lbox_scraper.scrape_data_from_film_entry(link)
+            lbox_scraper.implement_data_storage_options(film_data_dic)
+        if next_page == lbox_scraper.start_page + pages:
+            break
+        next_page_url = f'https://letterboxd.com/films/popular/size/small/page/{next_page}/'
+        lbox_scraper.driver.get(next_page_url)
+        print(f'Page {next_page} loaded.')
+        next_page += 1
+        
+    lbox_scraper.driver.quit()
 
 
 
